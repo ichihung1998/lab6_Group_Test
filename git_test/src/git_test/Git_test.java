3dae38d3f94968b58e61a4fe3c5bdd11d7676e4c
Student <Student@A125533.ad.csueastbay.edu>
2019-03-01 15:07:21 -0800

    u

--- git_test/src/git_test/Git_test.java
+++ git_test/src/git_test/Git_test.java
@@ -15,7 +15,7 @@ public class Git_test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello");
+        System.out.print("Hello from Hung");
     }
     
 }
